da6e80c0f15ea440a248a4ba1de0a7061872aa7a
Tom White <tom@sixdozen.com>
2017-03-16 17:30:02 +1300

    completed block font

--- clock.js
+++ clock.js
@@ -3,53 +3,154 @@
  */ 
 
 /* size of square */
-var s = 20
+var s = 30
+
+var numbers = [
+  // 0
+  [
+    [2, 1],
+    [3, 1],
+    [1, 2],
+    [4, 2],
+    [1, 3],
+    [4, 3],
+    [1, 4],
+    [4, 4],
+    [2, 5],
+    [3, 5],
+  ],
+  // 1
+  [
+    [2, 2],
+    [3, 1],
+    [3, 2],
+    [3, 3],
+    [3, 4],
+    [3, 5]
+  ],
+  // 2
+  [
+    [1, 1],
+    [2, 1],
+    [3, 1],
+    [4, 2],
+    [4, 3],
+    [3, 3],
+    [2, 4],
+    [1, 5],
+    [2, 5],
+    [3, 5],
+    [4, 5]
+  ],
+  [ // 3
+    [1, 1],
+    [2, 1],
+    [3, 1],
+    [4, 2],
+    [3, 3],
+    [2, 3],
+    [4, 4],
+    [3, 5],
+    [2, 5],
+    [1, 5]
+  ],
+  [ // 4
+    [1, 1],
+    [3, 1],
+    [1, 2],
+    [3, 2],
+    [1, 3],
+    [2, 3],
+    [3, 3],
+    [4, 3],
+    [3, 4],
+    [3, 5]
+  ],
+  [ // 5
+    [1, 1],
+    [2, 1],
+    [3, 1],
+    [4, 1],
+    [1, 2],
+    [1, 3],
+    [2, 3],
+    [3, 3],
+    [4, 4],
+    [3, 5],
+    [2, 5],
+    [1, 5]
+  ],
+  [ // 6
+    [2, 1],
+    [3, 1],
+    [4, 1],
+    [1, 2],
+    [1, 3],
+    [2, 3],
+    [3, 3],
+    [1, 4],
+    [4, 4],
+    [2, 5],
+    [3, 5],
+  ], //7
+  [
+    [1, 1],
+    [2, 1],
+    [3, 1],
+    [4, 1],
+    [4, 2],
+    [3, 3],
+    [3, 4],
+    [3, 5],
+  ],
+  [ // 8
+    [2, 1],
+    [3, 1],
+    [1, 2],
+    [4, 2],
+    [2, 3],
+    [3, 3],
+    [1, 4],
+    [4, 4],
+    [2, 5],
+    [3, 5],
+  ],
+  [ // 9
+    [2, 1],
+    [3, 1],
+    [4, 1],
+    [1, 2],
+    [4, 2],
+    [2, 3],
+    [3, 3],
+    [4, 3],
+    [4, 4],
+    [4, 5]
+  ],
+]
 
 function draw_number(num, x, y) {
   /* this resets any previous translations */
   resetMatrix();
   translate(x, y);
-  if (num === 1) {
-    rect(s * 3, s * 2, s, s);
-    rect(s * 4, s * 1, s, s);
-    rect(s * 4, s * 2, s, s);
-    rect(s * 4, s * 3, s, s);
-    rect(s * 4, s * 4, s, s);
-    rect(s * 4, s * 5, s, s);
-  }
-  else if (num === 2) {
-    rect(s * 1, s * 1, s, s);
-    rect(s * 2, s * 1, s, s);
-    rect(s * 3, s * 1, s, s);
-    rect(s * 4, s * 2, s, s);
-    rect(s * 4, s * 3, s, s);
-    rect(s * 3, s * 3, s, s);
-    rect(s * 2, s * 4, s, s);
-    rect(s * 1, s * 5, s, s);
-    rect(s * 2, s * 5, s, s);
-    rect(s * 3, s * 5, s, s);
-    rect(s * 4, s * 5, s, s);
-  }
-  else if (num === 3) {
-    rect(s * 1, s * 1, s, s);
-    rect(s * 2, s * 1, s, s);
-    rect(s * 3, s * 1, s, s);
-    rect(s * 4, s * 1, s, s);
-    rect(s * 4, s * 2, s, s);
-    rect(s * 4, s * 3, s, s);
-    rect(s * 3, s * 3, s, s);
-    rect(s * 2, s * 3, s, s);
-    rect(s * 4, s * 4, s, s);
-    rect(s * 4, s * 5, s, s);
-    rect(s * 3, s * 5, s, s);
-    rect(s * 2, s * 5, s, s);
-    rect(s * 1, s * 5, s, s);
+  var pixels = numbers[num%numbers.length];
+  for(var i=0; i<13; i++) {
+    var cur_pixel = pixels[i%pixels.length];
+    rect(cur_pixel[0] * s, cur_pixel[1] * s, s, s);
   }
 }
 
 function draw_clock(hour, minute, second, millis, alarm) {
   background(204);
-  draw_number(1, 10, 10);
-  draw_number(2, 130, 10);
-  draw_number(3, 250, 10);
+  draw_number(0, 10, 10);
+  draw_number(1, 10 + 6 * s, 10);
+  draw_number(2, 10 + 12 * s, 10);
+  draw_number(3, 10 + 18 * s, 10);
+  draw_number(4, 10 + 24 * s, 10);
+
+  draw_number(5, 10, 10 + 7 * s);
+  draw_number(6, 10 + 6 * s, 10 + 7 * s);
+  draw_number(7, 10 + 12 * s, 10 + 7 * s);
+  draw_number(8, 10 + 18 * s, 10 + 7 * s);
+  draw_number(9, 10 + 24 * s, 10 + 7 * s);
 }
